0833715dcda6e8215ac1991eca58cf0ca64a30bd
12 <77175547+mihai12p@users.noreply.github.com>
2021-11-19 12:22:57 +0200

    Update exp 1.c

--- c/exp 1.c
+++ c/exp 1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void afisare(int d, int (*sir)[d])
+void afisare(int d, int (*mat)[d])
 {
     for (int i = 0; i < d; ++i)
     {
         for (int j = 0; j < d; ++j)
-            printf("%d ", *(*(sir + i) + j));
+            printf("%d ", *(*(mat + i) + j));
         printf("\n");
     }
 }
@@ -13,12 +13,12 @@ void afisare(int d, int (*sir)[d])
 int main()
 {
     int d = 2;
-    int sir[d][d];
+    int mat[d][d];
     for (int i = 0; i < d; ++i)
         for (int j = 0; j < d; ++j)
-            scanf("%d", *(sir + i) + j);
+            scanf("%d", *(mat + i) + j);
 
-    afisare(d, sir);
+    afisare(d, mat);
 
     return 0;
 }

4dd3a23e23ab3a58465c0923d6ecbe1ecda36814
12 <unchnoun@gmail.com>
2022-10-23 13:20:54 +0300

    lab 1

---
+++ sem3/plf/P1/10a.pl
@@ -0,0 +1,30 @@
+%                          {
+%                           [E], L = []
+%inserareNAux(E, L, N, P) = [E + L], N = P
+%                           [L], altfel
+%                          }
+%Lista=Element*
+%inserareNAux(E:Element, L:Lista, N:Intreg, P:Intreg, R:Lista)
+%Modele de flux: (i, i, i, i, o), (i, i, i, i, i)
+%E - elementul pe care il inseram in lista
+%L - lista initiala
+%N - pozitia pe care vrem sa inseram elementul
+%P - pozitia elementului curent
+%R - lista finala dupa inserare
+inserareNAux(E, [], _, _, [E]).
+inserareNAux(E, L, N, P, [E | L]) :- N is P.
+inserareNAux(E, [H | T], N, P, R) :- inserareNAux(E, T, N, P + 1, R1),
+                                     R = [H | R1].
+
+%                    {
+%inserareN(E, L, N) = inserareNAux(E, L, N, 1)
+%                    }
+%Lista=Element*
+%inserareN(E:Element, L:Lista, N:Intreg, R:Lista) - determinist
+%Modele de flux: (i, i, i, o), (i, i, i, i)
+%E - elementul pe care il inseram in lista
+%L - lista initiala
+%N - pozitia pe care vrem sa inseram elementul
+%R - lista finala dupa inserare
+inserareN(E, L, N, R) :- inserareNAux(E, L, N, 1, R),
+                         !.
---
+++ sem3/plf/P1/10b.pl
@@ -0,0 +1,25 @@
+%             {
+%              A, B = 0
+%cmmdc(A, B) = B, A % B = 0
+%              cmmdc(B, A % B), altfel
+%             }
+%cmmdc(A:Intreg, B:Intreg, D:Intreg)
+%Modele de flux: (i, i, o), (i, i, i)
+%A - primul numar
+%B - al doilea numar
+%D - cel mai mare divizor comun al celor doua numere
+cmmdc(A, 0, A) :- !.
+cmmdc(A, B, D) :- R is A mod B,
+                  cmmdc(B, R, D).
+
+%                 {
+%cmmdcL(l1...ln) = cmmdcL(cmmdc(l1, l2)...ln)
+%                 }
+%Lista=Intreg*
+%cmmdcL(L:Lista, D:Intreg)
+%Modele de flux: (i, o), (i, i)
+%L - lista de numere
+%D - cel mai mare divizor comun al numerelor din lista
+cmmdcL([H | []], H) :- !.
+cmmdcL([H1, H2 | T], D) :- cmmdc(H1, H2, R),
+                           cmmdcL([R | T], D).
